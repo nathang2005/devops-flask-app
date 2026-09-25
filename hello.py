21104e49298fe67d4f984df38a411c0bc1c5c3c8
nathang2005 <c24380043@mytudublin.ie>
2026-09-25 11:37:48 +0100

    added /about

--- hello.py
+++ hello.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-    return '<p>Hello, World, I am a Flask app!</p>'
+    return '<p>Hello, World, I am a Flask app!</p><p><a href="/about">About this application</a></p>'
+
+@app.route('/about')
+def about():
+    return '<p>This application is running on the Flask web framework.</a>.</p>'
